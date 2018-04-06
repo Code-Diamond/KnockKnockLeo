e4ecbc042780c77cf458315e73f68d4387a03989
JaminROCK <KingJamin@hotmail.com>
2018-04-05 21:48:41 -0600

    Update Program.cs
    Added more to the story....

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace KnockKnockLeo
         static void Main(string[] args)
         {
             string story = "Hello Leo, follow the white rabbit. . .";
-
             A(story);
             story = "Knock knock Leo. . .";
             A("\n");
             A(story);
-            
+            B();
+            Console.ReadKey();
 
         }
 
@@ -27,9 +27,50 @@ namespace KnockKnockLeo
             {
                 Console.Write(a[i]);
                 Thread.Sleep(200);
+            }        
+        }
+        public static void C(string a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+                Thread.Sleep(1);
+
             }
-            
-            
+        }
+        public static void B()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Random rand = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                char[] lowerCase = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+                char[] upperCase = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+                char[] specialChar = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
+                int randomCharType = rand.Next(4);
+                int randomNumber;
+                switch (randomCharType)
+                {
+
+                    case 0:
+                        randomNumber = rand.Next(10);
+                        C(numbers[randomNumber] + "   ");
+                        break;
+                    case 1:
+                        randomNumber = rand.Next(26);
+                        C(lowerCase[randomNumber] + " ");
+                        break;
+                    case 2:
+                        randomNumber = rand.Next(26);
+                        C(upperCase[randomNumber] + "  ");
+                        break;
+                    case 3:
+                        randomNumber = rand.Next(12);
+                        C(specialChar[randomNumber] + "");
+                        break;
+                }
+            }
         }
     }
 }
